d5f2f996f91ab67c6076303a4d932c29e73672bc
helookme <azinard@outlook.com>
2026-02-27 18:31:23 +0800

    Update 7 files: cover1.webp, MasterMusic.js, bgm.mp3, APlayer.svelte, Music.astro, Layout.astro, [...page].astro

--- public/lib/MasterMusic.js
+++
@@ -1,9 +0,0 @@
-const musics = [
-    {
-        name: 'April Encounter',
-        artist: '很美味',
-        url: '/music/bgm.mp3',
-        cover: '/images/cover1.webp'
-    },
-];
-export default musics;
