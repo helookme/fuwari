32d59b598fab387688bf52fdc5f8e6c40cb084f2
helookme <azinard@outlook.com>
2026-03-07 12:41:36 +0800

    Update 4 files: config.ts, friends.ts, friends.astro, links.astro

--- src/config.ts
+++ src/config.ts
@@ -45,7 +45,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Archive,
 	    { 
             name: "友链", 
-            url: "/links" 
+            url: "/friends" 
         },
         { 
             name: "追番", 
--- src/data/friends.ts
+++ src/data/friends.ts
@@ -2,14 +2,14 @@
 export const friends = [
   {
     name: "AkiNard Blog",
-    url: "https://blog.91787878.xyz",
-    avatar: "https://image.91787878.xyz/img/skyceria.jpg",
+    url: "https://blog.578113.xyz",
+    avatar: "https://image.578113.xyz/img/skyceria.jpg",
     description: "其实就是本站啦~",
   },
     {
     name: "依凌の手册",
     url: "https://blog.hanahime.online",
-    avatar: "https://image.91787878.xyz/img/friend1.png",
+    avatar: "https://image.578113.xyz/img/friend1.png",
     description: "某个笨蛋的站点",
   },
 ];
