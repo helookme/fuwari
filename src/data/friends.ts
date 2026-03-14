8aabd6094f650432d3f748eae54443ee6d004a41
helookme <azinard@outlook.com>
2026-03-14 17:48:24 +0800

    Update 1 file: friends.ts

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -1,15 +1,21 @@
 // src/data/frend-links.ts
 export const friends = [
-  {
-    name: "AkiNard Blog",
-    url: "https://blog.578113.xyz",
-    avatar: "https://image.578113.xyz/img/skyceria.jpg",
-    description: "其实就是本站啦~",
-  },
     {
     name: "依凌の手册",
     url: "https://blog.hanahime.online",
     avatar: "https://image.578113.xyz/img/friend1.png",
     description: "某个笨蛋的站点",
+  },
+    {
+    name: "1zyq1's Blog",
+    url: "https://www.1zyq1.com",
+    avatar: "https://image.578113.xyz/img/friend2.png",
+    description: "Protect What You Love./爱你所爱！",
+  },
+      {
+    name: "Ad_closeNN 的小站",
+    url: "https://blog.adclosenn.top",
+    avatar: "https://blog.adclosenn.top/assets/avatar.jpg",
+    description: "Ad_closeNN 的小站，时不时会刷新一些野生东西",
   },
 ];
