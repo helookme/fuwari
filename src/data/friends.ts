91fa8a862844b39341d808bbd81548e26f81aa1f
helookme <azinard@outlook.com>
2026-03-14 19:23:17 +0800

    Update 1 file: friends.ts

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -7,12 +7,6 @@ export const friends = [
     description: "某个笨蛋的站点",
   },
     {
-    name: "1zyq1's Blog",
-    url: "https://www.1zyq1.com",
-    avatar: "https://image.578113.xyz/img/friend2.png",
-    description: "Protect What You Love./爱你所爱！",
-  },
-      {
     name: "Ad_closeNN 的小站",
     url: "https://blog.adclosenn.top",
     avatar: "https://blog.adclosenn.top/assets/avatar.jpg",
