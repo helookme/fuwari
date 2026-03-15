56a6732ed84a783ebc78033571fce486d9bd985b
alexma233 <i@fur.im>
2026-03-15 12:21:00 +0800

    fix: improve site accessibility

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -144,9 +144,9 @@ $: if (initialized && keywordMobile) {
       dark:bg-white/5 dark:hover:bg-white/10 dark:focus-within:bg-white/10
 ">
     <Icon icon="material-symbols:search" class="absolute text-[1.25rem] pointer-events-none ml-3 transition my-auto text-black/30 dark:text-white/30"></Icon>
-    <input placeholder="{i18n(I18nKey.search)}" bind:value={keywordDesktop} on:focus={() => search(keywordDesktop, true)}
+    <input type="search" name="site-search" placeholder={i18n(I18nKey.search)} aria-label={i18n(I18nKey.search)} bind:value={keywordDesktop} on:focus={() => search(keywordDesktop, true)}
            class="transition-all pl-10 text-sm bg-transparent outline-0
-         h-full w-40 active:w-60 focus:w-60 text-black/50 dark:text-white/50"
+          h-full w-40 active:w-60 focus:w-60 text-black/50 dark:text-white/50"
     >
 </div>
 
@@ -166,9 +166,9 @@ top-20 left-4 md:left-[unset] right-4 shadow-2xl rounded-2xl p-2">
       dark:bg-white/5 dark:hover:bg-white/10 dark:focus-within:bg-white/10
   ">
         <Icon icon="material-symbols:search" class="absolute text-[1.25rem] pointer-events-none ml-3 transition my-auto text-black/30 dark:text-white/30"></Icon>
-        <input placeholder="Search" bind:value={keywordMobile}
-               class="pl-10 absolute inset-0 text-sm bg-transparent outline-0
-               focus:w-60 text-black/50 dark:text-white/50"
+        <input type="search" name="site-search-mobile" placeholder={i18n(I18nKey.search)} aria-label={i18n(I18nKey.search)} bind:value={keywordMobile}
+                class="pl-10 absolute inset-0 text-sm bg-transparent outline-0
+                focus:w-60 text-black/50 dark:text-white/50"
         >
     </div>
 
