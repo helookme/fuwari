b5ab14630640b555cca6136e47778fca3380a024
helookme <azinard@outlook.com>
2026-03-15 16:38:34 +0800

    文章浏览量

--- src/config.ts
+++ src/config.ts
@@ -111,4 +111,13 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 	// Note: Some styles (such as background color) are being overridden, see the astro.config.mjs file.
 	// Please select a dark theme, as this blog theme currently only supports dark background color
 	theme: "github-dark",
+	
+};
+
+export const umamiConfig: UmamiConfig = {
+       enable: true,
+       baseUrl: "https://umami.578113.xyz",
+       shareId: "5ESTs8jIv3PziJdE",
+       timezone: "Asia/Shanghai",
+       
 };
